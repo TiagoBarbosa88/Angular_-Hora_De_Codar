6f8cff422c80608c9beb3562a7afc888dafdd613
TiagoBarbosa88 <sombr43@gmail.com>
2022-12-07 22:13:53 -0300

    Componentes e interpolação

---
+++ curso_angular/src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent implements OnInit {
+  name: string = "Tiago";
+  age: number = 34;
+  job = "Programador";
+  hobbies= [ 'Correr', ' Jogar', ' Estudar', ' Assistir'];
+  car = {
+    name: "Polo",
+    year: 2020,
+
+  }
+
+  constructor() {}
+
+    ngOnInit(): void {
+      //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+      //Add 'implements OnInit' to the class.
+
+    }
+
+
+}
