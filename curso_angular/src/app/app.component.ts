03a0d82a316f4411b05bd7e172e5abcb8a0f701b
TiagoBarbosa88 <sombr43@gmail.com>
2022-12-13 22:19:31 -0300

    Two way Binding

--- curso_angular/src/app/app.component.ts
+++ curso_angular/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  userName = "Joaquim";
+  name = "Joaquim";
   userHelp = 'Tiago';
 
   userData = {
