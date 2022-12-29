4e6aba9efeb47d0db6b5e3c77869c43cb981a3f7
TiagoBarbosa88 <sombr43@gmail.com>
2022-12-29 08:15:10 -0300

    validation form

--- moments/src/app/components/moment-form/moment-form.component.ts
+++ moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-moment-form',
@@ -6,12 +7,36 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./moment-form.component.css']
 })
 export class MomentFormComponent implements OnInit {
-@Input() btnText!: string
+  @Input() btnText!: string
+
+  momentForm!: FormGroup
 
 
   constructor() { }
 
   ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl('')
+     })
+  }
+
+  get title(){
+    return this.momentForm.get('title')!;
+  }
+
+  get description(){
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if(this.momentForm.invalid){
+      return;
+    }
+    console.log('Enviou');
+
   }
 
 }
