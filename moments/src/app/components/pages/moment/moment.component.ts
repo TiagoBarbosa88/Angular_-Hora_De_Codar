775f244ec5cdc9e27cf6f54ddf9be66ae634532e
TiagoBarbosa88 <sombr43@gmail.com>
2023-01-06 07:50:20 -0300

    css moment

--- moments/src/app/components/pages/moment/moment.component.ts
+++ moments/src/app/components/pages/moment/moment.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { MomentService } from 'src/app/services/moment.service';
 import { Moment } from 'src/app/Moment';
 import { Router, ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
@@ -11,6 +13,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class MomentComponent implements OnInit {
   moment?: Moment
+  baseApiUrl = environment.baseApiUrl
+  faTimes = faTimes
+  faEdit = faEdit
 
   constructor(
     private momentService: MomentService,
